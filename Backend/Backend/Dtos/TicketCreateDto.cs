a2da66b829ee092da50ae978d81eabff252639bd
brachi <b0548403177@gmail.com>
2026-04-13 16:09:36 +0300

    first commit

--- Backend/Backend/Dtos/TicketCreateDto.cs
+++ Backend/Backend/Dtos/TicketCreateDto.cs
@@ -5,6 +5,6 @@
         public string FullName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public string ImagePath { get; set; }
+        public IFormFile? ImageFile { get; set; }
     }
-}
+    }
--- Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0392dd45a6eb95dd9c6c676a6c580d0ff99ba7c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
